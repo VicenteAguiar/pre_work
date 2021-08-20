ed1bd59be95a2046167556d900f2fbfd5ab53bad
Nilaguia <eleniltonvicentedeaguiar@gmail.com>
2021-08-20 19:22:02 -0300

    Desafio do terceiro dia - Segundo exercício

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,3 +1,5 @@
+// exercicio 1
+
 const proibido = ['da', 'das', 'de', 'do', 'dos'];
 
 const inputName = document.querySelector('[data-js="inputName"]');
@@ -16,3 +18,35 @@ function repla(word) {
         return n.replace(n[0], n.charAt(0).toUpperCase())
     }).join(' ')
 }
+
+
+// exercicio 2
+const cores = ['yellow', 'red', 'black', 'blue', 'green']
+
+const form = document.querySelector('form')
+const select = document.createElement('select')
+const novaDivCores = document.createElement('div')
+novaDivCores.style.display = 'flex'
+
+select.addEventListener('change', (e) => {
+    novaDivCores.innerHTML = ''
+    Array.from(e.target.selectedOptions).map(el => {
+        const div = document.createElement('div')
+        div.style.width = '100px'
+        div.style.height = '100px'
+        div.style.background = el.value
+        console.log(el.value)
+        novaDivCores.appendChild(div)
+    })
+})
+
+cores.forEach(cor => {
+    const opt = document.createElement('option')
+    opt.value = cor
+    opt.textContent = cor
+    select.appendChild(opt)
+})
+
+select.setAttribute('multiple', '')
+form.appendChild(select)
+form.appendChild(novaDivCores)
