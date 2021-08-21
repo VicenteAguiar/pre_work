ad6460dc86937e5bf2c0a7bbdab2ab13bcdaaf2b
Nilaguia <eleniltonvicentedeaguiar@gmail.com>
2021-08-21 19:31:41 -0300

    Desafio do quarto dia - Segundo exercício (corrigido alguns erros - erro ao listar os carros)

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,32 +1,78 @@
-import './style.css'
+import "./style.css";
 
-const baseUrl = 'http://localhost:3333/cars'
+const baseUrl = "http://localhost:3333/cars";
 
 // pego os elementos do form
 const pegarElemento = (evento) => (elemento) => {
-    return evento.target.elements[elemento]
-}
+    return evento.target.elements[elemento];
+};
 
 // ciando os elementos da tabela
-const bodyTable = document.querySelector('[data-js="tbody"]')
-const rowBodyTable = document.createElement('tr')
-const dataBodyTable = document.createElement('td')
-const createDadosTable = () => {}
+const bodyTable = document.querySelector('[data-js="tbody"]');
+const createDadosTable = () => {};
 
-function dataTable() {
-    return fetch(baseUrl)
-        .then(result => result.json())
+function createElementInTable(element, valor) {
+    const el = document.createElement(element);
+    el.textContent = valor;
+    const ref = el;
+    return ref;
 }
 
-dataTable()
-    .then(result => {
+function dataTable() {
+    return fetch(baseUrl).then((result) => result.json());
+}
+
+function edit() {
+    const btnEdit = createElementInTable("button");
+    const edit = createElementInTable("i");
+    btnEdit.appendChild(edit);
+    edit.classList.add("fas", "fa-edit");
+
+    const btnTrash = createElementInTable("button");
+    const trash = createElementInTable("i");
+    btnTrash.appendChild(trash);
+    trash.classList.add("fas", "fa-trash");
+    btnTrash.classList.add("btn", "btn-danger", "m-1");
+
+    const editTD = createElementInTable("td");
+    btnEdit.classList.add("btn", "btn-info");
+    editTD.appendChild(btnEdit);
+    editTD.appendChild(btnTrash);
+
+    return editTD;
+}
+
+dataTable().then(
+    (result) => {
         if (result.length === 0) {
-            dataBodyTable.textContent = "Nenhum carro encontrado!"
-            dataBodyTable.setAttribute('colspan', '6')
-            dataBodyTable.classList.add('text-center', 'bg-warning')
-            bodyTable.appendChild(dataBodyTable)
-            return
+            const el = createElementInTable("td", "Nenhum carro encontrado!");
+            el.setAttribute("colspan", "6");
+            el.classList.add("text-center", "bg-warning");
+            bodyTable.appendChild(el);
+            return;
+        } else {
+            for (let data in result) {
+                const el = createElementInTable("tr");
+                const image = createElementInTable("td", `${result[data].plate}`);
+                const brandModel = createElementInTable(
+                    "td",
+                    `${result[data].brandModel}`
+                );
+                const year = createElementInTable("td", `${result[data].year}`);
+                const plate = createElementInTable("td", `${result[data].plate}`);
+                const color = createElementInTable("td", `${result[data].color}`);
+                const editTD = edit();
+
+                const items = [image, brandModel, year, plate, color, editTD];
+                items.forEach((n) => {
+                    el.appendChild(n);
+                });
+
+                bodyTable.appendChild(el);
+            }
         }
-    }, (error) => {
-        console.log('Error: ', error)
-    })
+    },
+    (error) => {
+        console.log("Surgiu um error inesperado!", error);
+    }
+);
