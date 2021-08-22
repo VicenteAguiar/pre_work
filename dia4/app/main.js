17142d25839129d69a1983c45601904e6be5bb9e
Nilaguia <eleniltonvicentedeaguiar@gmail.com>
2021-08-21 21:57:13 -0300

    Desafio do quarto dia - Terceiro exercício - Cadastrar um carro

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,6 +1,8 @@
 import "./style.css";
 
 const baseUrl = "http://localhost:3333/cars";
+const form = document.querySelector('[data-js="car"]')
+const warn = document.querySelector('[data-js="warning"]')
 
 // pego os elementos do form
 const pegarElemento = (evento) => (elemento) => {
@@ -42,37 +44,99 @@ function edit() {
     return editTD;
 }
 
-dataTable().then(
-    (result) => {
-        if (result.length === 0) {
-            const el = createElementInTable("td", "Nenhum carro encontrado!");
-            el.setAttribute("colspan", "6");
-            el.classList.add("text-center", "bg-warning");
-            bodyTable.appendChild(el);
-            return;
-        } else {
-            for (let data in result) {
-                const el = createElementInTable("tr");
-                const image = createElementInTable("td", `${result[data].plate}`);
-                const brandModel = createElementInTable(
-                    "td",
-                    `${result[data].brandModel}`
-                );
-                const year = createElementInTable("td", `${result[data].year}`);
-                const plate = createElementInTable("td", `${result[data].plate}`);
-                const color = createElementInTable("td", `${result[data].color}`);
-                const editTD = edit();
+function createElementImage(srcImage, altImag) {
+    const td = createElementInTable('td')
+    const img = createElementInTable('img')
+    img.src = srcImage
+    img.setAttribute('alt', altImag)
+    img.classList.add('img-fluid')
+    td.appendChild(img)
+    return td
+}
 
-                const items = [image, brandModel, year, plate, color, editTD];
-                items.forEach((n) => {
-                    el.appendChild(n);
-                });
-
+const showData = () => {
+    dataTable().then(
+        (result) => {
+            if (result.length === 0) {
+                const el = createElementInTable("td", "Nenhum carro encontrado!");
+                el.setAttribute("colspan", "6");
+                el.classList.add("text-center", "bg-warning");
                 bodyTable.appendChild(el);
+                return;
+            } else {
+                for (let data in result) {
+                    const el = createElementInTable("tr");
+                    const image = createElementImage(`${result[data].image}`, `Imagem da api`);
+                    const brandModel = createElementInTable(
+                        "td",
+                        `${result[data].brandModel}`
+                    );
+                    const year = createElementInTable("td", `${result[data].year}`);
+                    const plate = createElementInTable("td", `${result[data].plate}`);
+                    const color = createElementInTable("td", `${result[data].color}`);
+                    const editTD = edit();
+
+                    const items = [image, brandModel, year, plate, color, editTD];
+                    items.forEach((n) => {
+                        el.appendChild(n);
+                    });
+
+                    bodyTable.appendChild(el);
+                }
             }
+        },
+        (error) => {
+            toasts("Surgiu um error inesperado!", error);
         }
-    },
-    (error) => {
-        console.log("Surgiu um error inesperado!", error);
+    )
+}
+
+
+// cadastrar os carros
+function registerCar(dados) {
+    const register = fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: dados
+    }).then(res => res.json())
+    return register
+}
+
+function toasts(msg) {
+    warn.textContent = msg
+    warn.removeAttribute('hidden', 'hidden')
+    return warn
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const el = pegarElemento(e)
+    const image = el('image').value
+    const brandModel = el('marca').value
+    const year = el('ano').value
+    const plate = el('placa').value
+    const color = el('cor').value
+
+    const dados = {
+        image,
+        brandModel,
+        year,
+        plate,
+        color
     }
-);
+
+
+    registerCar(JSON.stringify(dados))
+        .then(data => {
+            toasts(`${data.message}`)
+        })
+    bodyTable.textContent = ''
+    document.location.reload(true)
+    form.reset()
+    form.image.focus()
+
+})
+showData()
+setInterval(() => { warn.setAttribute('hidden', 'hidden') }, 5000)
